4cb4125ca39f9f6da2a00ac012b8d3770289d373
Maria Eberechi Kalu Ugwu <kaluugwumaria@gmail.com>
2026-02-07 19:25:35 +0200

    Update support email

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2087046791"
+    serviceWorkerVersion: "2117838641"
   }
 });
--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "26437724916143c002407ecd8fef3555
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d4bc12d49eb2809f46f769c13edd5a04",
+"flutter_bootstrap.js": "917e6230783121950dc9df29efe49817",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
